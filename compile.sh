85923b9446a0bdd551cc87aaa7361cbe25b85984
Fatkullin <fatkullinrustam@gmail.com>
2014-03-10 11:56:37 +0400

    Подправил скрипт compile.sh так, чтобы он создавал папку bin и очищал ее перед каждой сборкой.

--- compile.sh
+++ compile.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+mkdir -p bin
+rm -f bin/*
 cd bin
+
 flex ../frontend/lexer.l
 bison -d ../frontend/parser.y
 g++ -g -I. -I../frontend/ -I../backend -o compiler lex.yy.c parser.tab.c ../backend/compiler.h ../backend/compiler.cpp
